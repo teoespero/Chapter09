d466485c767a6deb9891532cc1f1afe81d6d83d8
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-01-27 18:49:48 -0800

    Add files via upload

---
+++ PointerSample/PointerSample.cpp
@@ -0,0 +1,30 @@
+//  PointerSample.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//  Teodulfo Espero
+//  BS Cloud and Systems Administration
+//  BS Software Development
+//  Western Governors University
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	int mynumber = 0,
+		*mypointer = &mynumber;
+
+	cout << "enter a number... ";
+	cin >> mynumber;
+
+	cout << "mynumber...." << mynumber << endl;
+	cout << "*mypointer....." << *mypointer << endl;
+
+	cout << endl << "enter a new value for *mypointer.....";
+	cin >> *mypointer;
+
+	cout << endl << "new values" << endl;
+	cout << "mynumber...." << mynumber << endl;
+	cout << "*mypointer....." << *mypointer << endl;
+}
